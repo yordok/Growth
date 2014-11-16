83706eca247a6eafa49702a3cdc71e76ca4e4e06
BKenny11 <bwk8958@rit.edu>
2014-11-16 12:56:08 -0500

    Update PlayerController.cs

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
 	public Vector2 moving = new Vector2();
 	// Use this for initialization
 	void Start () {
@@ -12,5 +11,16 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		moving.x = moving.y = 0;
+		if (Input.GetKey ("right")) {
+			moving.x = 1;
+		} else if (Input.GetKey ("left")) {
+			moving.x = -1;
+		}
+
+		if (Input.GetKey ("up")) {
+			moving.y = 1;
+		}else if(Input.GetKey("down")){
+			moving.y = -1;
+		}
 	}
 }
