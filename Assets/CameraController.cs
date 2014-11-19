dec5b23cbcaf8001a7129f0ed2c1eebee99c14e7
BKenny11 <bwk8958@rit.edu>
2014-11-19 10:15:50 -0500

    Create CameraController.cs

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject target; // reference to the game object the camera will follow
+	public float speed = 1.0f; // speed constant
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find ("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target != null) {
+			transform.position = new Vector3 (target.transform.position.x, target.transform.position.y, transform.position.z); // apply depth
+	
+		}
+	}
+}
