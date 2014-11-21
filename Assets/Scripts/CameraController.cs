12539c4fdfdc91b96497f9cb1bd5bff1cc35e5d9
BKenny11 <bwk8958@rit.edu>
2014-11-21 10:19:00 -0500

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,13 @@
+//GameObjectFind set "player" to the name of the in scene player GameObject
+
 using UnityEngine;
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
-
+	
 	public GameObject target; // reference to the game object the camera will follow
 	public float speed = 1.0f; // speed constant
-
+	
 	// Use this for initialization
 	void Start () {
 		target = GameObject.Find ("player");
@@ -15,7 +17,7 @@ public class CameraController : MonoBehaviour {
 	void Update () {
 		if (target != null) {
 			transform.position = new Vector3 (target.transform.position.x, target.transform.position.y, transform.position.z); // apply depth
-	
+			
 		}
 	}
 }
